8c5085be5ad008dc50ddc4e4f54fb614aa52df34
Emre Kuru <emre.kuru@ozu.edu.tr>
2024-05-14 22:17:33 +0300

    feat: Plor the parse trees for debugging

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import sys
 from antlr4 import *
 from ANTLR.PrologLexer import PrologLexer
 from ANTLR.PrologParser import PrologParser
+from antlr4.tree.Tree import ParseTreeWalker
+from antlr4.tree.Trees import Trees
+from graphviz import Digraph
+
+def add_nodes_edges(t, dot, parser):
+    text = Trees.getNodeText(t, ruleNames=parser.ruleNames)
+    if t.getChildCount() == 0:
+        dot.node(str(id(t)), text)
+    else:
+        dot.node(str(id(t)), text)
+        for child in t.getChildren():
+            dot.edge(str(id(t)), str(id(child)))
+            add_nodes_edges(child, dot, parser)
 
 def main():
     input_stream = FileStream("test.pl") 
@@ -11,6 +24,10 @@ def main():
     
     tree = parser.p_text()
     print(tree.toStringTree(recog=parser))
+
+    dot = Digraph(comment='Prolog Parse Tree')
+    add_nodes_edges(tree, dot, parser)
+    dot.render('graphs/parse_tree.gv', view=True)  # Saves and opens the file
 
 if __name__ == "__main__":
     main()
